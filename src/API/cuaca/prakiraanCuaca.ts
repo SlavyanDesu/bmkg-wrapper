a3acc9a5ad9c981d9f00bae540a663e1543cb545
Heikal Syah Shiddiq <heikalsyahshiddiq@gmail.com>
2025-03-29 01:33:17 +0700

    wip: error `prakiraanCuaca()`

--- src/API/cuaca/prakiraanCuaca.ts
+++ src/API/cuaca/prakiraanCuaca.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { XMLParser } from 'fast-xml-parser';
-import { getWeatherData } from '../../util/functions.js';
-import type { Cuaca } from '../../util/interfaces.js';
-import { baseUrl, endpoints } from '../../util/variables.js';
+import { getWeatherData } from '../../utils/helper.js';
+import type { Cuaca } from '../../utils/interfaces.js';
+import { baseUrl, endpoints } from '../../utils/variables.js';
 const parser = new XMLParser({ ignoreAttributes: false });
 
 /**
@@ -12,21 +12,19 @@ const parser = new XMLParser({ ignoreAttributes: false });
  * @returns {Promise<Cuaca>} Object prakiraan cuaca untuk `daerah`.
  */
 export async function prakiraanCuaca(daerah = 'indonesia'): Promise<Cuaca> {
-  try {
-    let found = false;
-    Object.keys(endpoints.cuaca).forEach((key) => {
-      if (key == daerah.toLowerCase()) {
-        daerah = key;
-        found = true;
-      }
-    });
-    if (found) {
-      const res = await axios.get(baseUrl.cuaca + endpoints.cuaca[daerah as keyof typeof endpoints.cuaca]);
-      return getWeatherData(parser.parse(res.data));
-    } else {
-      throw new Error('Data cuaca tidak ditemukan!');
-    }
-  } catch (err) {
-    throw err;
-  }
+	let found = false;
+	Object.keys(endpoints.cuaca).forEach((key) => {
+		if (key == daerah.toLowerCase()) {
+			daerah = key;
+			found = true;
+		}
+	});
+	if (found) {
+		const res = await axios.get(
+			baseUrl.cuaca + endpoints.cuaca[daerah as keyof typeof endpoints.cuaca]
+		);
+		return getWeatherData(parser.parse(res.data));
+	} else {
+		throw new Error('Data cuaca tidak ditemukan!');
+	}
 }
---
+++ src/utils/helper.ts
@@ -0,0 +1,158 @@
+/* eslint-disable no-prototype-builtins */
+import axios from 'axios';
+import csv from 'csv-parser';
+import dayjs from 'dayjs';
+import type {
+	Cuaca,
+	CuacaDaerah,
+	KodeWilayah,
+	PrakiraanCuaca,
+} from './interfaces';
+
+/**
+ * Format string time dari `YYYYMMDDHHmm` ke format yang ditentukan.
+ *
+ * @param {string} date - String time dengan format `YYYYMMDDHHmm`.
+ * @param {string} [format = 'YYYY/MM/DD HH:mm'] - Format yang ditentukan. Default `YYYY/MM/DD HH:mm`.
+ * @returns {string} String time dengan bentuk `format`.
+ */
+export function timeFormatter(
+	date: string,
+	format = 'YYYY/MM/DD HH:mm'
+): string {
+	const stringTime = new Date(
+		date.replace(/^(\d{4})(\d\d)(\d\d)(\d\d)(\d\d)$/, '$4:$5:$6 $2/$3/$1')
+	);
+	return dayjs(stringTime).format(format);
+}
+
+/**
+ * Menerjemahkan kode cuaca ke kondisi cuaca dalam bentuk string.
+ *
+ * @param {number} code - Kode cuaca.
+ * @returns {string} String dari kode cuaca yang diberikan.
+ */
+export function weatherCodeToString(code: number): string {
+	const codes = {
+		0: 'Cerah',
+		1: 'Cerah Berawan',
+		2: 'Cerah Berawan',
+		3: 'Berawan',
+		4: 'Berawan Tebal',
+		5: 'Udara Kabur',
+		10: 'Asap',
+		45: 'Kabut',
+		60: 'Hujan Ringan',
+		61: 'Hujan Sedang',
+		63: 'Hujan Lebat',
+		80: 'Hujan Lokal',
+		95: 'Hujan Petir',
+		97: 'Hujan Petir',
+	};
+	let key = null;
+	Object.keys(codes).forEach((keys) => {
+		if (code == Number(keys)) {
+			key = keys;
+		}
+	});
+	if (key != null) {
+		return codes[key];
+	} else {
+		return 'Kode cuaca tidak dapat diterjemahkan.';
+	}
+}
+
+/**
+ * Mengambil data cuaca dari XML yang telah di-parse menjadi object.
+ *
+ * @param {any} parsedXML - XML yang telah di-parse menjadi object.
+ * @returns {Cuaca} Array object prakiraan cuaca tiap daerah.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function getWeatherData(parsedXML: any): Cuaca {
+	const weatherArray: PrakiraanCuaca[] = [],
+		areaArray: CuacaDaerah[] = [];
+	const { issue, ...rest } = parsedXML.data.forecast;
+	for (let i = 0; i < rest.area.length; i++) {
+		if (!rest.area[i].hasOwnProperty('parameter')) break;
+		for (let j = 0; j < 12; j++) {
+			weatherArray.push({
+				waktu: timeFormatter(
+					rest.area[i].parameter[6].timerange[j]['@_datetime']
+				),
+				cuaca: weatherCodeToString(
+					rest.area[i].parameter[6].timerange[j].value['#text']
+				),
+				suhu: {
+					celsius: {
+						data: rest.area[i].parameter[5].timerange[j].value[0]['#text'],
+					},
+					fahrenheit: {
+						data: rest.area[i].parameter[5].timerange[j].value[1]['#text'],
+					},
+				},
+				angin: {
+					kecepatan_angin: {
+						knot: rest.area[i].parameter[8].timerange[j].value[0]['#text'],
+						mph: rest.area[i].parameter[8].timerange[j].value[1]['#text'],
+						kph: rest.area[i].parameter[8].timerange[j].value[2]['#text'],
+						ms: rest.area[i].parameter[8].timerange[j].value[3]['#text'],
+					},
+					arah_angin: {
+						derajat: rest.area[i].parameter[7].timerange[j].value[0]['#text'],
+						card: rest.area[i].parameter[7].timerange[j].value[1]['#text'],
+						sexa: rest.area[i].parameter[7].timerange[j].value[2]['#text'],
+					},
+				},
+				kelembapan: {
+					data: rest.area[i].parameter[0].timerange[j].value['#text'],
+				},
+			});
+		}
+		areaArray.push({
+			kota: rest.area[i].name[0]['#text'],
+			prakiraan_cuaca: weatherArray,
+		});
+	}
+	const obj: Cuaca = {
+		timestamp: {
+			tahun: issue.year,
+			bulan: issue.month,
+			tanggal: issue.day,
+			jam: issue.hour,
+			menit: issue.minute,
+			detik: issue.second,
+		},
+		daerah: areaArray,
+	};
+	return obj;
+}
+
+export const fetchCsv = async (url: string): Promise<KodeWilayah[]> => {
+	const response = await axios.get(url, { responseType: 'stream' });
+	try {
+		return new Promise((resolve, reject) => {
+			const results: KodeWilayah[] = [];
+			response.data
+				.pipe(csv({ headers: false }))
+				.on('data', (row: Record<string, string>) => {
+					const kode = row[0];
+					const wilayah = row[1];
+
+					if (kode && wilayah) results.push({ kode, wilayah });
+				})
+				.on('end', () => resolve(results))
+				.on('error', (err: unknown) => reject(err));
+		});
+	} catch (err) {
+		console.error(err);
+		return [];
+	}
+};
+
+export const searchByValue = (data: KodeWilayah[], value: string) => {
+	const index = data.findIndex(
+		(item) => item.wilayah.toLowerCase() === value.toLowerCase()
+	);
+	return data[index];
+};
